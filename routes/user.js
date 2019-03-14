2b43d7d5f5244c4d4ebc5c088e3d411c1dc18ac0
Anthony Alfaro <tonyfaros@gmail.com>
2019-03-14 10:25:03 -0600

    init

--- routes/user.js
+++ routes/user.js
@@ -75,11 +75,11 @@ router.get('/users', (req, res) => {
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: '127.0.0.1',
-    port: '3306',
-    user: 'tony',
-    password: '1234',
-    database:'berthatemp1'
+    host: 'us-cdbr-iron-east-03.cleardb.net',
+    //port: '3306',
+    user: 'b2422c79d1fa8f',
+    password: '8649bbd4',
+    database:'heroku_cd69aac1f1eff94'
 
 })
 
