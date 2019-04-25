7c43d48859c8c960cb3875de277ba197ff8384bc
Anthony Alfaro <tonyfaros@gmail.com>
2019-04-25 14:12:32 -0600

    cars fixed sp

--- routes/cars/index.js
+++ routes/cars/index.js
@@ -8,7 +8,8 @@ router.use(bodyParser.urlencoded({extended: false}))
 router.use(express.static('././public'))
 
 router.post('/car_create/', (req, res) =>{
-    
+
+    const _id_user = req.body._id_user
     const make = req.body.make
     const model = req.body.model
     const year = req.body.year
@@ -20,22 +21,25 @@ router.post('/car_create/', (req, res) =>{
     const length_mm = req.body.length_mm
     const width_mm = req.body.width_mm
     const height_mm = req.body.height_mm
-    const mpg_hwy = req.body.mpg_hwy       //HAY QUE CAMBIARLO
-    const mpg_city = req.body.mpg_city       //HAY QUE CAMBIARLO
-    const mpg_mixed = req.body.impg_mixedd       //HAY QUE CAMBIARLO
+    const lkm_hwy = req.body.lkm_hwy       
+    const lkm_city = req.body.lkm_city       
+    const lkm_mixed = req.body.lkm_mixed       
     const body_style = req.body.body_style
     const door_number = req.body.door_number
     const drive = req.body.drive
     const engine_position = req.body.engine_position
     const engine_type = req.body.engine_type
+    const car_transmission = req.body.car_transmission
+    const car_drive_conditions = req.body.car_drive_conditions
+    const car_tireSize = req.body.car_tireSize
 
     ///const queryString = "insert into cars (make, model,year,licence_plate,fuel_type, weigh_kg,model_trim,engine_cc, length_mm, width_mm,height_mm,mpg_hwy,mpg_city, mpg_mixed,body_style,door_number,drive,engine_position,engine_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
 
-    const queryString2 = "CALL `heroku_cd69aac1f1eff94`.`car_data`('','' ,? ,? ,? ,? ,? ,? ,?,? ,? ,? ,? ,? ,? , ?,? ,? ,? ,? ,? , 'new', @out_param);"
+    const queryString2 = "CALL `heroku_cd69aac1f1eff94`.`car_data`('',?,?, ?,? ,? ,? ,? ,? ,? ,?,? ,? ,? ,? ,? ,? ,? ,? ,? ,? ,? ,? , 'new', @out_param);"
 
   
-    getConnection().query(queryString2, [make,model,year,license_plate,fuel_type,weigh_kg,model_trim,engine_cc,length_mm,width_mm,height_mm
-        ,mpg_hwy,mpg_city,mpg_mixed,body_style,door_number,drive,engine_position,engine_type], (err,res,fields) => {
+    getConnection().query(queryString2, [_id_user,make,model,year,model_trim,license_plate,car_transmission,fuel_type,car_drive_conditions,car_tireSize,weigh_kg,engine_cc,length_mm,width_mm,height_mm
+        ,lkm_hwy,lkm_city,lkm_mixed,body_style,door_number,drive,engine_position,engine_type], (err,res,fields) => {
         if(err){
             console.log(err)
             console.log("ERROR")
