4ce029234f60da8c17cc6e8b6d80686a984f0d97
Anthony Alfaro <tonyfaros@gmail.com>
2019-06-16 00:01:39 -0600

    fixing groups

--- routes/challenges/index.js
+++ routes/challenges/index.js
@@ -61,7 +61,7 @@ challenges.post('/link_group_challenge/', (req, res) => {
     const query_addChallenge = "CALL `heroku_cd69aac1f1eff94`.`groups_x_challenge`( ?, ?, ?);"
 
     for(var i = 0; i < jsonLen; i++){
-        var key_groupTemp = "id_group"+1
+        var key_groupTemp = "id_group"+i
         console.log("Inside for: "+data[key_groupTemp])
 
         connection.query(query_addChallenge, [id_challengeSended,data.id_group+i,action], (err, rows)=>{
